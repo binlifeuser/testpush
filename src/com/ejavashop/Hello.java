173dce3983c8297089f776958af964880a7722a5
binlifeuser <bin_job@sina.com>
2017-05-21 16:54:32 +0800

    Update Hello.java

--- src/com/ejavashop/Hello.java
+++ src/com/ejavashop/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("helloword");
         System.out.println("我就是我");
+         System.out.println("我就是我，不一样的烟火");
 
     }
 }
